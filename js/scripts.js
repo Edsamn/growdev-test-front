ff68bafb055c5e6e4e14fa4aceb40b86957c1fdb
Eduardo Ribas <eduardomsribas@gmail.com>
2022-03-07 19:54:17 -0300

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-const lista = document.getElementById('lista');
-axios.get(8080)
-    .then(response => {
-        response.data.forEach(user => {
-            lista.innerHTML += `
+async function loadusers() {
+    try {
+        const lista = document.getElementById('lista');
+        axios.get('8080')
+            .then(response => {
+                response.data.forEach(user => {
+                    lista.innerHTML += `
                 <li>${user.name}<li>
             `;
-        });
-    });
+                });
+            });
 
-    .catch(error => {
-        console.log(error);
-    });
+    } catch (error) {
+        (error => console.log(error));
+    }
 
-axios.post('/users', {
-    name: 'Vini',
-    idade: 18,
-    email:''
-});
+
+    axios.post('/users', {
+        name: 'Vini',
+        idade: 18,
+        email: ''
+    });
+}
 
 axios.put('/users/1');
 axios.delete();   
